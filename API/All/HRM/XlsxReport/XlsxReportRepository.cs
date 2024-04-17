229ee34da158d0912cdc0905c0926ce68d0736d4
pkthong12 <pkthong12@gmail.com>
2024-04-18 02:21:30 +0700

    update

--- API/All/HRM/XlsxReport/XlsxReportRepository.cs
+++ API/All/HRM/XlsxReport/XlsxReportRepository.cs
@@ -850,6 +850,301 @@ namespace API.All.HRM.XlsxReport
                             ws.Cells[lastRow + 3, 13, lastRow + 3, 15].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
                             break;
                         }
+                    case "EMPLOYEES_EXPIRING_CONTRACTS_REPORT":
+                        {
+                            var referenceData = ds.Tables[0].ToList<EMPLOYEES_EXPIRING_CONTRACTS_REPORT>();
+                            rangeAddress = ExcelCellBase.GetAddress(config.HeaderRow + 1, 1, config.HeaderRow + (referenceData.Count != 0 ? referenceData.Count : 1), config.HeaderGridTitles.Count);
+                            var range = ws.Cells[rangeAddress];
+
+                            // Set borders content
+                            range.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                            range.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                            range.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                            range.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+
+                            // Đổ dữ liệu trả về từ SQL
+                            range.LoadFromCollection(referenceData, false);
+                            ws.Cells.AutoFitColumns();
+                            break;
+                        }
+                    case "STATISTICS_OF_WORKERS_BY_GENDER_AGE_REPORT":
+                        {
+                            var dataFromDb = ds.Tables[0].ToList<EMPLOYEE_GENDER_AGE>();
+
+                            var listDistinctOrg = dataFromDb.DistinctBy(x => x.ORG_ID).ToList();
+
+                            // tạo danh sách 2 chiều để chứa dữ liệu
+                            List<List<EMPLOYEE_GENDER_AGE>> list_2_dimensional = new List<List<EMPLOYEE_GENDER_AGE>>();
+
+                            foreach (var item in listDistinctOrg)
+                            {
+                                var findAllRecord = dataFromDb.Where(x => x.ORG_ID == item.ORG_ID).ToList();
+
+                                if (findAllRecord is not null && findAllRecord.Count > 0) list_2_dimensional.Add(findAllRecord);
+                            }
+
+                            // declare listResult is final handle
+                            List<MODEL_RESULT> listResult = new List<MODEL_RESULT>();
+
+                            int stt = 0;
+
+                            MODEL_RESULT endTemp = new MODEL_RESULT()
+                            {
+                                ORG_NAME = "Tổng",
+                                TOTAL_NUMBER = 0,
+                                COUNT_MALE = 0,
+                                COUNT_FEMALE = 0,
+                                COUNT_AGE_18_TO_30 = 0,
+                                COUNT_AGE_31_TO_35 = 0,
+                                COUNT_AGE_36_TO_40 = 0,
+                                COUNT_AGE_41_TO_50 = 0,
+                                COUNT_AGE_51_TO_60 = 0,
+                                COUNT_AGE_GREATER_THAN_60 = 0
+                            };
+
+                            foreach (var item in list_2_dimensional)
+                            {
+                                stt++;
+
+                                int countTotalNumber = item.Count;
+
+                                int countMale = item.Count(x => x.GENDER_CODE == "N1");
+                                int countFemale = item.Count(x => x.GENDER_CODE == "N2");
+
+                                decimal percentMale = Math.Floor((decimal)countMale / countTotalNumber * 100 * 100) / 100;
+                                decimal percentFemale = (decimal)100 - percentMale;
+
+                                int count18to30 = item.Count(x => now.Year - x.BIRTH_DATE!.Value.Year >= 18 && now.Year - x.BIRTH_DATE!.Value.Year <= 30);
+                                decimal percent18to30 = Math.Floor((decimal)count18to30 / countTotalNumber * 100 * 100) / 100;
+
+                                int count31to35 = item.Count(x => now.Year - x.BIRTH_DATE!.Value.Year >= 31 && now.Year - x.BIRTH_DATE!.Value.Year <= 35);
+                                decimal percent31to35 = Math.Floor((decimal)count31to35 / countTotalNumber * 100 * 100) / 100;
+
+                                int count36to40 = item.Count(x => now.Year - x.BIRTH_DATE!.Value.Year >= 36 && now.Year - x.BIRTH_DATE!.Value.Year <= 40);
+                                decimal percent36to40 = Math.Floor((decimal)count36to40 / countTotalNumber * 100 * 100) / 100;
+
+                                int count41to50 = item.Count(x => now.Year - x.BIRTH_DATE!.Value.Year >= 41 && now.Year - x.BIRTH_DATE!.Value.Year <= 50);
+                                decimal percent41to50 = Math.Floor((decimal)count41to50 / countTotalNumber * 100 * 100) / 100;
+
+                                int count51to60 = item.Count(x => now.Year - x.BIRTH_DATE!.Value.Year >= 51 && now.Year - x.BIRTH_DATE!.Value.Year <= 60);
+                                decimal percent51to60 = Math.Floor((decimal)count51to60 / countTotalNumber * 100 * 100) / 100;
+
+                                int countGreaterThan60 = item.Count(x => now.Year - x.BIRTH_DATE!.Value.Year > 60);
+                                decimal percentGreaterThan60 = 100 - percent18to30 - percent31to35 - percent36to40 - percent41to50 - percent51to60;
+
+
+                                // fix percent for many column
+                                if (countGreaterThan60 == 0 && percentGreaterThan60 != 0)
+                                {
+                                    // remaining percent
+                                    decimal remainingPercent = percentGreaterThan60;
+
+                                    percentGreaterThan60 = 0;
+
+                                    if (percent51to60 != 0)
+                                    {
+                                        percent51to60 = percent51to60 + remainingPercent;
+                                    }
+                                    else if (percent51to60 == 0)
+                                    {
+                                        if (percent41to50 != 0)
+                                        {
+                                            percent41to50 = percent41to50 + remainingPercent;
+                                        }
+                                        else if (percent41to50 == 0)
+                                        {
+                                            if (percent36to40 != 0)
+                                            {
+                                                percent36to40 = percent36to40 + remainingPercent;
+                                            }
+                                            else if (percent36to40 == 0)
+                                            {
+                                                if (percent31to35 != 0)
+                                                {
+                                                    percent31to35 = percent31to35 + remainingPercent;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+
+
+                                MODEL_RESULT temp = new MODEL_RESULT();
+
+
+                                temp.STT = stt;
+                                temp.ORG_NAME = item[0].ORG_NAME;
+                                temp.TOTAL_NUMBER = countTotalNumber;
+
+                                temp.COUNT_MALE = countMale;
+                                temp.PERCENT_MALE = percentMale;
+
+                                temp.COUNT_FEMALE = countFemale;
+                                temp.PERCENT_FEMALE = percentFemale;
+
+                                temp.COUNT_AGE_18_TO_30 = count18to30;
+                                temp.PERCENT_AGE_18_TO_30 = percent18to30;
+
+                                temp.COUNT_AGE_31_TO_35 = count31to35;
+                                temp.PERCENT_AGE_31_TO_35 = percent31to35;
+
+                                temp.COUNT_AGE_36_TO_40 = count36to40;
+                                temp.PERCENT_AGE_36_TO_40 = percent36to40;
+
+                                temp.COUNT_AGE_41_TO_50 = count41to50;
+                                temp.PERCENT_AGE_41_TO_50 = percent41to50;
+
+                                temp.COUNT_AGE_51_TO_60 = count51to60;
+                                temp.PERCENT_AGE_51_TO_60 = percent51to60;
+
+                                temp.COUNT_AGE_GREATER_THAN_60 = countGreaterThan60;
+                                temp.PERCENT_AGE_GREATER_THAN_60 = percentGreaterThan60;
+
+
+                                listResult.Add(temp);
+
+
+                                endTemp.TOTAL_NUMBER = endTemp.TOTAL_NUMBER + countTotalNumber;
+                                endTemp.COUNT_MALE = endTemp.COUNT_MALE + countMale;
+                                endTemp.COUNT_FEMALE = endTemp.COUNT_FEMALE + countFemale;
+                                endTemp.COUNT_AGE_18_TO_30 = endTemp.COUNT_AGE_18_TO_30 + count18to30;
+                                endTemp.COUNT_AGE_31_TO_35 = endTemp.COUNT_AGE_31_TO_35 + count31to35;
+                                endTemp.COUNT_AGE_36_TO_40 = endTemp.COUNT_AGE_36_TO_40 + count36to40;
+                                endTemp.COUNT_AGE_41_TO_50 = endTemp.COUNT_AGE_41_TO_50 + count41to50;
+                                endTemp.COUNT_AGE_51_TO_60 = endTemp.COUNT_AGE_51_TO_60 + count51to60;
+                                endTemp.COUNT_AGE_GREATER_THAN_60 = endTemp.COUNT_AGE_GREATER_THAN_60 + countGreaterThan60;
+                            }
+
+                            // assign STT to endRecord
+                            endTemp.STT = stt + 1;
+                            endTemp.PERCENT_MALE = Math.Floor(Convert.ToDecimal((decimal)endTemp.COUNT_MALE! / endTemp.TOTAL_NUMBER * 100 * 100)) / 100;
+                            endTemp.PERCENT_FEMALE = (decimal)100 - endTemp.PERCENT_MALE;
+
+                            endTemp.PERCENT_AGE_18_TO_30 = Math.Floor((decimal)((decimal)endTemp.COUNT_AGE_18_TO_30 / endTemp.TOTAL_NUMBER * 100 * 100)) / 100;
+                            endTemp.PERCENT_AGE_31_TO_35 = Math.Floor((decimal)((decimal)endTemp.COUNT_AGE_31_TO_35 / endTemp.TOTAL_NUMBER * 100 * 100)) / 100;
+                            endTemp.PERCENT_AGE_36_TO_40 = Math.Floor((decimal)((decimal)endTemp.COUNT_AGE_36_TO_40 / endTemp.TOTAL_NUMBER * 100 * 100)) / 100;
+                            endTemp.PERCENT_AGE_41_TO_50 = Math.Floor((decimal)((decimal)endTemp.COUNT_AGE_41_TO_50 / endTemp.TOTAL_NUMBER * 100 * 100)) / 100;
+                            endTemp.PERCENT_AGE_51_TO_60 = Math.Floor((decimal)((decimal)endTemp.COUNT_AGE_51_TO_60 / endTemp.TOTAL_NUMBER * 100 * 100)) / 100;
+
+                            if (endTemp.COUNT_AGE_GREATER_THAN_60 == 0)
+                            {
+                                endTemp.PERCENT_AGE_GREATER_THAN_60 = 0;
+                            }
+                            else
+                            {
+                                endTemp.PERCENT_AGE_GREATER_THAN_60 = (decimal)100 - endTemp.PERCENT_AGE_18_TO_30 -
+                                                                                 endTemp.PERCENT_AGE_31_TO_35 -
+                                                                                 endTemp.PERCENT_AGE_36_TO_40 -
+                                                                                 endTemp.PERCENT_AGE_41_TO_50 -
+                                                                                 endTemp.PERCENT_AGE_51_TO_60;
+                            }
+
+                            listResult.Add(endTemp);
+
+
+                            int countColumnInExcel = 19;
+
+                            var referenceData = listResult;
+                            rangeAddress = ExcelCellBase.GetAddress(config.HeaderRow + 1, 1, config.HeaderRow + (referenceData.Count != 0 ? referenceData.Count : 1), countColumnInExcel);
+                            var range = ws.Cells[rangeAddress];
+
+                            // Set borders content
+                            range.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                            range.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                            range.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                            range.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+
+                            // fill data return from Database
+                            range.LoadFromCollection(referenceData, false);
+                            ws.Cells.AutoFitColumns();
+
+
+
+                            // set column width
+                            ws.Column(1).Width = 4;
+                            // ws.Column(2).Width = 16;
+                            ws.Column(3).Width = 6;
+
+                            // declare row and col
+                            short row1, col1;
+                            short row2, col2;
+
+                            // Header 1
+                            row1 = 1;
+                            col1 = 1;
+                            row2 = 2;
+                            col2 = 3;
+                            ws.Cells[row1, col1].Value = "Tổng công ty Máy động lực và Máy nông nghiệp Việt Nam - CTCP";
+                            ws.Cells[row1, col1, row2, col2].Merge = true;
+                            ws.Cells[row1, col1, row2, col2].Style.Font.Bold = true;
+                            ws.Cells[row1, col1, row2, col2].Style.WrapText = true;
+                            ws.Cells[row1, col1, row2, col2].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                            ws.Cells[row1, col1, row2, col2].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                            ws.Row(row2).Height = 30;
+
+                            // Header 2
+                            row1 = 1;
+                            col1 = 15;
+                            row2 = 1;
+                            col2 = 19;
+                            ws.Cells[row1, col1].Value = "CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM";
+                            ws.Cells[row1, col1, row2, col2].Merge = true;
+                            ws.Cells[row1, col1, row2, col2].Style.Font.Bold = true;
+                            ws.Cells[row1, col1, row2, col2].Style.WrapText = false;
+                            ws.Cells[row1, col1, row2, col2].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                            ws.Cells[row1, col1, row2, col2].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+
+                            // Header 3
+                            row1 = 2;
+                            col1 = 15;
+                            row2 = 2;
+                            col2 = 19;
+                            ws.Cells[row1, col1].Value = "Độc lập - Tự do - Hạnh phúc";
+                            ws.Cells[row1, col1, row2, col2].Merge = true;
+                            ws.Cells[row1, col1, row2, col2].Style.Font.Bold = true;
+                            ws.Cells[row1, col1, row2, col2].Style.WrapText = false;
+                            ws.Cells[row1, col1, row2, col2].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                            ws.Cells[row1, col1, row2, col2].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+
+                            // Header 4
+                            row1 = 3;
+                            col1 = 1;
+                            row2 = 3;
+                            col2 = 19;
+                            ws.Cells[row1, col1].Value = "BÁO CÁO THỐNG KÊ LAO ĐỘNG THEO GIỚI TÍNH, TUỔI";
+                            ws.Cells[row1, col1, row2, col2].Merge = true;
+                            ws.Cells[row1, col1, row2, col2].Style.Font.Bold = true;
+                            ws.Cells[row1, col1, row2, col2].Style.WrapText = false;
+                            ws.Cells[row1, col1, row2, col2].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                            ws.Cells[row1, col1, row2, col2].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+
+                            // Header 5
+                            row1 = 4;
+                            col1 = 1;
+                            row2 = 4;
+                            col2 = 19;
+                            ws.Cells[row1, col1].Value = $"Ngày báo cáo: {request.ReportDate!.Value.ToString("dd/MM/yyyy")}";
+                            ws.Cells[row1, col1, row2, col2].Merge = true;
+                            ws.Cells[row1, col1, row2, col2].Style.Font.Bold = true;
+                            ws.Cells[row1, col1, row2, col2].Style.WrapText = false;
+                            ws.Cells[row1, col1, row2, col2].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                            ws.Cells[row1, col1, row2, col2].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+
+                            // render "SL" and "%"
+                            row1 = 8;
+                            col1 = 4;
+                            for (int i = 1; i <= 8; i++)
+                            {
+                                ws.Cells[row1, col1].Value = "SL";
+                                ws.Cells[row1, col1 + 1].Value = "%";
+                                col1 = (short)(col1 + 2);
+                            }
+
+                            // remove format bold of "SL" and "%"
+                            ws.Cells[8, 4, 8, 19].Style.Font.Bold = false;
+
+                            break;
+                        }
                     default:
                         break;
                 }
---
+++ API/Entities/REPORT/EMPLOYEES_EXPIRING_CONTRACTS_REPORT.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Entities.REPORT
+{
+    public class EMPLOYEES_EXPIRING_CONTRACTS_REPORT
+    {
+        public long STT { get; set; }
+        public string? CODE { get; set; }
+        public string? FULL_NAME { get; set; }
+        public string? POSITION { get; set; }
+        public string? ORG_NAME { get; set; }
+        public string? TYPE_CONTRACT { get; set; }
+        public string? START_DATE { get; set; }
+        public string? EXPIRE_DATE { get; set; }
+    }
+}
